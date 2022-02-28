f8986841b1f9a09f48a448e5dab6e4173bbb2ef6
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-02-27 22:26:09 -0300

    implementação knn completo

--- algoritmo-KNN/main.cpp
+++ algoritmo-KNN/main.cpp
@@ -56,8 +56,86 @@ std::string sortSample(std::vector<Individual>& individuals, Individual new_exam
 
     
     std::set<std::pair<double, int>> individuals_dist;
+
+
+    for (int i = 0; i < tam_vet; i++)
+    {
+        double dist = getEuclideanDistance(individuals[i], new_example);
+        individuals_dist.insert(std::make_pair(dist, i));
+    }
+
+    std::set<std::pair<double, int>>::iterator it;
+
+    std::vector<int> contClasses(3);
+    int contK = 0;
+
+    for ( it = individuals_dist.begin(); it != individuals_dist.end(); it++)
+    {
+        std::string classe = individuals[it->second].getClasse();
+
+        if(classe == "Iris-setosa")
+            contClasses[0]++;
+        else if(classe == "Iris-versicolor")
+            contClasses[1]++;
+        else
+            contClasses[2]++;
+
+        if (contK > k) break;
+
+        contK++;
+    }
+
+    std::string classeClassificacao;
+
+    if(contClasses[0] >= contClasses[1] && contClasses[0] >= contClasses[2])
+        classeClassificacao = "Iris-setosa";
+
+    else if(contClasses[1] >= contClasses[0] && contClasses[1] >= contClasses[2])
+        classeClassificacao = "Iris-versicolor";
+    else
+        classeClassificacao = "Iris-virginica";
+
+    return classeClassificacao;
 }
 
 int main(int argc, char const* argv[]) {
+    std::vector<Individual> individuals;
+
+    int k = 3;
+
+    int tamTreinamento = 105;
+
+    for(int i= 0; i < tamTreinamento; i++){
+        std::string classe;
+        double a, b, c, d;
+
+        std::cin >> a >> b >> c >> d >> classe;
+
+        individuals.push_back(Individual(a, b, c, d, classe));
+    }
+
+    int acertos = 0;
+    int tamTestes = 150 - tamTreinamento;
+
+    for (int i = 0; i < tamTestes; i++)
+    {
+        std::string classe;
+        double a, b, c, d;
+
+        std::cin >> a >> b >> c >> d >> classe;
+
+        Individual ind(a, b, c, d, classe);
+
+        std::string classeObtida = sortSample(individuals, ind, k);
+
+        std::cout << "Classe esperada : " << classe << "\n";
+        std::cout << "Classe obtida: " << classeObtida << "\n\n";
+
+        if(classe == classeObtida)
+            acertos++;
+    }
+
+    std::cout << acertos << " Acertos de um total de " << tamTestes << " testes \n\n";
+
     return 0;
 }
